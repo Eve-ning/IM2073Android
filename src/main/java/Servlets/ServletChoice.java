f9d73e042cbf0aa210d7c60c6ced7ef261e796d7
Evening <johnchangqi@hotmail.com>
2021-04-09 11:44:44 +0800

    Use username instead of userid

--- src/main/java/Servlets/ServletChoice.java
+++ src/main/java/Servlets/ServletChoice.java
@@ -1,5 +1,6 @@
 package Servlets;
 
+import DBHandler.DBHandler;
 import ServletBridge.ServletChoiceBridge;
 
 import jakarta.servlet.ServletException;
@@ -9,15 +10,26 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @WebServlet(name = "Servlets.ServletChoice", value = "/choice")
 public class ServletChoice extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("success", false);
         try {
+            String username = request.getParameter("username");
+            DBHandler dbHandler = new DBHandler();
+            ResultSet resultSet = dbHandler.executeQuery(
+                    "SELECT * FROM credentials WHERE username LIKE '" + username + "'");
+            if (!resultSet.isBeforeFirst()) {
+                throw new SQLException("Cannot find id associated with username, aborting.");
+            }
+
+            resultSet.next();
             boolean status = ServletChoiceBridge.addChoice(
-                    request.getParameter("userId"),
+                    resultSet.getString("id"),
                     request.getParameter("choice"),
                     request.getParameter("qNo")
             );
